c42c650af6c8f67e4fdbeb3946208377eaa65627
Leandro Nazareth <leandrosnazareth@gmail.com>
2025-09-25 21:11:45 -0300

    import

--- kafka-msg-api/src/main/java/com/leandrosnazareth/kafka_msg_api/service/SmsService.java
+++ kafka-msg-api/src/main/java/com/leandrosnazareth/kafka_msg_api/service/SmsService.java
@@ -2,8 +2,6 @@ package com.leandrosnazareth.kafka_msg_api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.leandrosnazareth.kafka_msg_api.model.Sms;
 import com.leandrosnazareth.kafka_msg_api.model.StatusSms;
